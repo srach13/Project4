e5cecbb4eb81ca9ab7fb3da76edcdc7aceced5d7
Sameera Rachakonda <srachakonda6@gmail.com>
2021-04-13 12:36:20 -0400

    Create processes for signal handling

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/time.h>
+#include <assert.h>
+#include <unistd.h>
+
+sharedCounters *sharedMem;
+FILE *log_file = NULL;
 
 int main() {
 
@@ -17,5 +21,27 @@ int main() {
     sharedMem->num_sig_1_received = 0;
     sharedMem->num_sig_2_received = 0;
     sharedMem->kill_flag = 0;
+
+    pid_t signal_gen_proc_1, signal_gen_proc_2, signal_gen_proc_3;
+    pid_t signal_handler_1, signal_handler_2, signal_handler_3, signal_handler_4;
+    pid_t reporter_proc;
+
+    // create 3 processes for signal generating
+    if ((signal_gen_proc_1 = fork()) == 0) {
+        signal_generator();
+    } else if ((signal_gen_proc_2 = fork()) == 0) {
+        signal_generator();
+    } else if ((signal_gen_proc_3 = fork()) == 0) {
+        signal_generator();
+
+    // create 2 processes for SIGUSR1 and 2 processes for SIGUSR2
+    } else if ((signal_handler_1 = fork()) == 0) {
+        signal_handler(SIGUSR1);
+    } else if ((signal_handler_2 = fork()) == 0) {
+        signal_handler(SIGUSR1);
+    } else if ((signal_handler_3 = fork()) == 0) {
+        signal_handler(SIGUSR2);
+    } else if ((signal_handler_4 = fork()) == 0) {
+        signal_handler(SIGUSR2);
 
 }
