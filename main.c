915dd158e07811125b32d2f7a12bfa0f629c2a63
Sameera Rachakonda <srachakonda6@gmail.com>
2021-04-13 15:25:25 -0400

    Cleaned up code & edited comments

--- main.c
+++ main.c
@@ -3,14 +3,19 @@
 #include <sys/time.h>
 #include <assert.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/shm.h>
+#include <pthread.h>
 
+#include "utilities.c"
 
-// FUNCTION PROTOTYPES
-void interval_clock();
+//FUNCTION PROTOTYPES
 void signal_generator();
 void signal_handler(int sig);
+void handler(int sig);
+void exit_handler(int sig);
 
-// STRUCT FOR sharedCounters
+//STRUCT FOR sharedCounters
 typedef struct sharedCounters {
     int num_sig_1_sent;
     int num_sig_2_sent;
@@ -24,20 +29,19 @@ typedef struct sharedCounters {
     pthread_mutex_t kill_lock;
 } sharedCounters;
 
-
 sharedCounters *sharedMem;
 FILE *log_file = NULL;
 
 int mode = 0;
 int sig_1_handled = 0, sig_2_handled = 0;
 
-// MAIN FUNCTION
+//MAIN FUNCTION
 int main() {
 
     int status;
     srand(time(NULL));
 
-    // created shared memory and attach it to the parent process
+    //created shared memory and attach it to the parent process
     int shm_id = shmget(IPC_PRIVATE, sizeof(sharedCounters), IPC_CREAT | 0666);
     assert(shm_id >= 0);
     sharedMem = (sharedCounters *) shmat(shm_id, NULL, 0);
@@ -51,7 +55,7 @@ int main() {
     pid_t signal_handler_1, signal_handler_2, signal_handler_3, signal_handler_4;
     pid_t reporter_proc;
 
-    // create 3 processes for signal generating
+    //create 3 processes for signal generating
     if ((signal_gen_proc_1 = fork()) == 0) {
         signal_generator();
     } else if ((signal_gen_proc_2 = fork()) == 0) {
@@ -59,7 +63,7 @@ int main() {
     } else if ((signal_gen_proc_3 = fork()) == 0) {
         signal_generator();
 
-        // create 2 processes for SIGUSR1 and 2 processes for SIGUSR2
+        //create 2 processes for SIGUSR1 and 2 processes for SIGUSR2
     } else if ((signal_handler_1 = fork()) == 0) {
         signal_handler(SIGUSR1);
     } else if ((signal_handler_2 = fork()) == 0) {
@@ -69,47 +73,65 @@ int main() {
     } else if ((signal_handler_4 = fork()) == 0) {
         signal_handler(SIGUSR2);
 
-        // create 1 process for reporting
+        //create 1 process for reporting
     } else if ((reporter_proc = fork()) == 0) {
-        reporter();
+        //reporter();
     }
 }
 
-// FUNCTION TO GET INTERVAL FOR SLEEP B/T 0.01 AND 0.1 SECONDS
-void interval_clock() {
-    srand(time(NULL));
-    int randint;
-    randint = rand() % (100000 + 1 - 10000) + 10000; // get random number between 0.01 and 0.01 seconds
-    usleep(randint);
-}
-
-// FUNCTION THAT GENERATES EITHER SIGUSR1 OR SIGUSR2
+//FUNCTION THAT GENERATES EITHER SIGUSR1 OR SIGUSR2
 void signal_generator() {
     int status;
     srand(time(NULL));
 }
 
-// FUNCTION WRAPPER FOR HANDLER
+//FUNCTION WRAPPER FOR HANDLER
 void signal_handler(int sig) {
 
     signal(SIGTERM, exit_handler);
 
-    // unblock the signal that this process is handling so that it can receive the signal
+    //unblock the signal that this process is handling so that it can receive the signal
     if (sig == SIGUSR1) unblock_sig1();
     if (sig == SIGUSR2) unblock_sig2();
 
-    // set the handler function for the specified signal
+    //set the handler function for the specified signal
     if (signal(sig, handler) == SIG_ERR) {
         fflush(stdout);
         printf("error setting handler!");
         return;
     }
 
-    // wait in a loop for signals
+    //wait for signals
     while (!sharedMem->kill_flag) {
         sleep(1);
     }
 
+    shmdt(sharedMem);
+    exit(EXIT_SUCCESS);
+}
+
+//FUNCTION FOR HANDLER
+void handler(int sig) {
+
+    fflush(stdout);
+
+    if (sig == SIGUSR1) {
+        //use locks
+        pthread_mutex_lock(&(sharedMem->sig_1_received_lock));
+        sharedMem->num_sig_1_received++;
+        pthread_mutex_unlock(&(sharedMem->sig_1_received_lock));
+
+    } else if (sig == SIGUSR2) {
+        //use locks
+        pthread_mutex_lock(&(sharedMem->sig_2_received_lock));
+        sharedMem->num_sig_2_received++;
+        pthread_mutex_unlock(&(sharedMem->sig_2_received_lock));
+    }
+}
+
+//FUNCTION TO EXIT HANDLER
+void exit_handler(int sig) {
+    if (log_file != NULL) fclose(log_file);
     shmdt(sharedMem);
     exit(EXIT_SUCCESS);
 }
--- utilities.c
+++ utilities.c
@@ -1,24 +1,15 @@
 #include "main.c"
 
-void block_sigs() {
-    sigset_t sigset;
-    sigemptyset(&sigset); // initalize set to empty
-    sigaddset(&sigset, SIGUSR1); // add SIGUSR1 to set
-    sigaddset(&sigset, SIGUSR2); // add SIGUSR2 to
-    sigaddset(&sigset, SIGINT); // add SIGUSR2 to
-    sigprocmask(SIG_BLOCK, &sigset, NULL); // modify mask
-}
-
 void unblock_sig1() {
     sigset_t sigset;
-    sigemptyset(&sigset); // initalize set to empty
-    sigaddset(&sigset, SIGUSR1); // add SIGUSR1 to set
-    sigprocmask(SIG_UNBLOCK, &sigset, NULL); // modify mask
+    sigemptyset(&sigset); //initialize set to empty
+    sigaddset(&sigset, SIGUSR1); //add SIGUSR1 to set
+    sigprocmask(SIG_UNBLOCK, &sigset, NULL); //modify mask
 };
 
 void unblock_sig2() {
     sigset_t sigset;
-    sigemptyset(&sigset); // initalize set to empty
-    sigaddset(&sigset, SIGUSR2); // add SIGUSR2 to set
-    sigprocmask(SIG_UNBLOCK, &sigset, NULL); // modify mask
+    sigemptyset(&sigset); //initialize set to empty
+    sigaddset(&sigset, SIGUSR2); //add SIGUSR2 to set
+    sigprocmask(SIG_UNBLOCK, &sigset, NULL); //modify mask
 };
