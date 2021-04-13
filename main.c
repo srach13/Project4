42ae2596c3b8b6444ea975a19b08a1ec909de1b6
Sameera Rachakonda <srachakonda6@gmail.com>
2021-04-13 12:33:39 -0400

    Create shared memory in main function

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+    int status;
+    srand(time(NULL));
+
+    // created shared memory and attach it to the parent process
+    int shm_id = shmget(IPC_PRIVATE, sizeof(sharedCounters), IPC_CREAT | 0666);
+    assert(shm_id >= 0);
+    sharedMem = (sharedCounters *) shmat(shm_id, NULL, 0);
+    sharedMem->num_sig_1_sent = 0;
+    sharedMem->num_sig_2_sent = 0;
+    sharedMem->num_sig_1_received = 0;
+    sharedMem->num_sig_2_received = 0;
+    sharedMem->kill_flag = 0;
+
 }
