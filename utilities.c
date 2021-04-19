c9edd931c1b9b5769fe78c4edf7501c96aba4cb9
Sameera Rachakonda <srachakonda6@gmail.com>
2021-04-18 21:29:04 -0400

    Create individual locks for each counter

--- utilities.c
+++ utilities.c
@@ -13,3 +13,36 @@ void unblock_sig2() {
     sigaddset(&sigset, SIGUSR2); //add SIGUSR2 to set
     sigprocmask(SIG_UNBLOCK, &sigset, NULL); //modify mask
 };
+
+void init_locks(sharedCounters *sharedMem) {
+    // initialize mutex locks
+    if (pthread_mutex_init(&(sharedMem->sig_1_received_lock), NULL) != 0) {
+        fflush(stderr);
+        fprintf(stderr, "error: failed to initialize sig_1_received_lock");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_mutex_init(&(sharedMem->sig_2_received_lock), NULL) != 0) {
+        fflush(stderr);
+        fprintf(stderr, "error: failed to initialize sig_2_received_lock");
+        exit(EXIT_FAILURE);
+    };
+
+    if (pthread_mutex_init(&(sharedMem->sig_1_sent_lock), NULL) != 0) {
+        fflush(stderr);
+        fprintf(stderr, "error: failed to initialize sig_1_sent_lock");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_mutex_init(&(sharedMem->sig_2_sent_lock), NULL) != 0) {
+        fflush(stderr);
+        fprintf(stderr, "error: failed to initialize sig_2_sent_lock");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_mutex_init(&(sharedMem->kill_lock), NULL) != 0) {
+        fflush(stderr);
+        fprintf(stderr, "error: failed to initialize sig_2_sent_lock");
+        exit(EXIT_FAILURE);
+    }
+}
